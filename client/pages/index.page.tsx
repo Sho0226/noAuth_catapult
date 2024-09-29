db95fc7603c9fd79e7deff2cf0295b7b0a34b2cd
Sho0226 <s1F102201616@iniad.org>
2024-09-30 02:04:45 +0900

    タスクを削除

--- client/pages/index.page.tsx
+++ client/pages/index.page.tsx
@@ -1,11 +1,9 @@
-import { TaskList } from 'features/tasks/TaskList';
 import styles from './index.module.css';
 
 const Home = () => {
   return (
     <div className={styles.container}>
       <div className={styles.title}>Hello !</div>
-      <TaskList />
     </div>
   );
 };
